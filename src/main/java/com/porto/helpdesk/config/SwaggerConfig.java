94a4db7942cb1438397f2356d706b87ee9b44071
Jefferson da Silva Souza <jeffersonsilva.souza@portoseguro.com.br>
2024-10-08 20:52:58 -0300

    Update Swager Security

--- src/main/java/com/porto/helpdesk/config/SwaggerConfig.java
+++ src/main/java/com/porto/helpdesk/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.porto.helpdesk.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +19,10 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(new Info()
                         .title("Spring HelpDesk API")
                         .description("Spring HelpDesk application")
@@ -27,6 +33,12 @@ public class SwaggerConfig {
                         .description("SpringShop Wiki Documentation")
                         .url("https://springshop.wiki.github.org/docs"));
 
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
     @Bean
